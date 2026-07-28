c0e9bb9b00e71b5498014567bc561d7431473cda
risingfirework <rising.firework@gmail.com>
2026-07-28 22:07:12 +0700

    frontend-only-branch

--- backend/models/AcademicYear.js
+++
@@ -1,7 +0,0 @@
-const { JsonDB } = require('../config/db');
-
-const AcademicYearDB = new JsonDB('academic_years');
-
-module.exports = {
-  db: AcademicYearDB
-};
--- backend/models/LapgiatMedia.js
+++
@@ -1,7 +0,0 @@
-const { JsonDB } = require('../config/db');
-
-const LapgiatMediaDB = new JsonDB('lapgiat_media');
-
-module.exports = {
-  db: LapgiatMediaDB
-};
